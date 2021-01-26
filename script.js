b45a6eca5077d21bd2419cacc18dd33c80a59214
Andrew Burke <Drewburke39@gmail.com>
2021-01-25 19:48:50 -0700

    added two ajax calls to js

--- script.js
+++ script.js
@@ -2,9 +2,24 @@ var searchButton = $("#searchButton");
 var searchInput = $("#searchInput");
 var queryURL = "https://opentdb.com/api.php?amount=10";
 
+
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
     console.log(response)
-})
+});
+
+$.ajax({
+    url: "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC",
+    method: "GET"
+}).then(function (response) {
+    console.log(response)
+});
+$.ajax({
+    url: "https://api.fungenerators.com/taunt/generate?category=new-age-insult&limit=3",
+    method: "GET",
+    contentType: "application/json",
+}).then(function (response) {
+    console.log(response)
+});
