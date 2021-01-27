4cf2bec40a03e0a43b98a1757dc1587302107a57
E-Martinez77 <emartizzle2185@me.com>
2021-01-26 18:09:54 -0700

    added variables for new divs

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-
-var searchButton = $("#searchButton");
-var searchInput = $("#searchInput");
 var queryURL = "https://opentdb.com/api.php?amount=10";
 
+var scoreValue = $("#scoreValue");
+var questionDiv = $("#questionDiv");
+var answer1 = $("#answer1");
+var answer2 = $("#answer2");
+var answer3 = $("#answer3");
+var answer4 = $("#answer4");
 
 $.ajax({
-    url: queryURL,
-    method: "GET"
+  url: queryURL,
+  method: "GET",
 }).then(function (response) {
-    console.log(response)
+  console.log(response);
 });
 
 $.ajax({
-    url: "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC",
-    method: "GET"
+  url: "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC",
+  method: "GET",
 }).then(function (response) {
-    console.log(response)
+  console.log(response);
 });
-$.ajax({
-    url: "https://api.fungenerators.com/taunt/generate?category=new-age-insult&limit=3",
-    method: "GET",
-    contentType: "application/json",
-}).then(function (response) {
-    console.log(response)
-});
+// $.ajax({
+//     url: "https://api.fungenerators.com/taunt/generate?category=new-age-insult&limit=3",
+//     method: "GET",
+//     contentType: "application/json",
+// }).then(function (response) {
+//     console.log(response)
+// });
