a44c7343ba53742a014a96c70b0b88d240f153ad
Andrew Burke <Drewburke39@gmail.com>
2021-01-30 15:52:40 -0700

    reverted back to code before shuffle

--- click-function.js
+++ click-function.js
@@ -6,32 +6,71 @@ var incorrect1 = "";
 var incorrect2 = "";
 var incorrect3 = "";
 var questions = "";
+var score = 0;
+var nextQuestion = "";
+
 // var btn1 = document.getElementById("#answer1");
 // var btn2 = document.getElementById("#answer2");
 // var btn3 = document.getElementById("#answer3");
 // var btn4 = document.getElementById("#answer4");
-
+// var index = 0;
 var btn1 = $("#answer1");
 var btn2 = $("#answer2");
 var btn3 = $("#answer3");
 var btn4 = $("#answer4");
+
 $.ajax({
     url: queryURL,
     method: "GET",
 
 }).then(function (response) {
+    var response = response;
+    // JSON.parse(response.replace(/&quot;/g, '"'));
     var index = 0;
-    questions = response.results[index].question;
+    questions = response.results[index].question
     correctAnswers = response.results[index].correct_answer;
     incorrect1 = response.results[index].incorrect_answers[0];
     incorrect2 = response.results[index].incorrect_answers[1];
     incorrect3 = response.results[index].incorrect_answers[2];
-    btn1.append(correctAnswers);
-    btn2.append(incorrect1);
-    btn3.append(incorrect2);
-    btn4.append(incorrect3);
+    maxQuestions = 10;
+    console.log(response)
+    // var answer = [correctAnswers, incorrect1, incorrect2, incorrect3]
+    // shuffle(answer);
+    // for (let j = 1; j <= answer.length; j++) {
+    //     $("#answer" + [j]).text(answer[j])
 
-    var answer = [btn1, btn2, btn3, btn4]
+
+    // }
+    // btn1.append(correctAnswers);
+    // btn2.append(incorrect1);
+    // btn3.append(incorrect2);
+    // btn4.append(incorrect3);
+    // console.log(questions + "this is line 40")
+    function quizQuestions() {
+        score = 0;
+        nextQuestion = 0;
+        // questions;
+        console.log(questions + " this is inside quizQuestions function")
+    }
+    function getNextQuestion() {
+        if (response.results.indexOf(response.results[index]) === 0 || nextQuestion <= maxQuestions)
+            index++;
+        console.log(index)
+        console.log(questions + "this is Questions incremented ")
+        questions = response.results[index].question;
+        correctAnswers = response.results[index].correct_answer;
+        incorrect1 = response.results[index].incorrect_answers[0];
+        incorrect2 = response.results[index].incorrect_answers[1];
+        incorrect3 = response.results[index].incorrect_answers[2];
+        maxQuestions = 10;
+        $("#questionDiv").text(questions)
+        btn1.text(correctAnswers);
+        btn2.text(incorrect1);
+        btn3.text(incorrect2);
+        btn4.text(incorrect3);
+    }
+
+    var answer = [correctAnswers, incorrect1, incorrect2, incorrect3]
     console.log(answer);
     shuffle(answer);
     console.log(response);
@@ -43,12 +82,22 @@ $.ajax({
         console.log(choices)
 
     }
+    // function shuffle(answer) {
+    //     console.log(answer.sort(() => Math.random() - 0.5));
+    // }
+    $("button").on("click", function () {
+        getNextQuestion()
+        quizQuestions()
 
+    })
 });
 
-$("button").click(function(){
-    alert("test")
-})
+// $("button").click(function () {
+//     alert("test")
+//     getNextQuestion()
+//     quizQuestions()
+
+// })
 
 // $.ajax({
 //     url: "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC",
@@ -65,8 +114,7 @@ $("button").click(function(){
 //     console.log(response)
 // });
 
-function shuffle(array) {
-    console.log(array.sort(() => Math.random() - 0.5));
+function shuffle(answer) {
+    console.log(answer.sort(() => Math.random() - 0.5));
 }
-
 // shuffle([1, 2, 3, 4, 5])
